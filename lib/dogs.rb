ccdc26d9392efd901728fa2e29115dab30e632c7
Luke McComb <lucasmccomb@gmail.com>
2014-06-25 16:01:28 -0600

    working on to_s test and changes Sarah's last name to Darnum

--- lib/dogs.rb
+++ lib/dogs.rb
@@ -28,6 +28,86 @@ class Dogs
     ]
   end
 
-  # only edit below this line
+  def small_dogs
+    result = []
+    @dogs.select do |elem|
+      if elem[:size] == :small
+        result.push({name: elem[:name]})
+      end
+    end
+    result
+  end
+
+  def huge_dog
+    result = []
+    @dogs.select do |elem|
+      if elem[:size] == :huge
+       result.push({name: elem[:name]})
+      end
+    end
+    result
+  end
+
+  def large_dog_names
+    result = []
+    @dogs.select do |elem|
+      if elem[:size] == :large
+        result << elem[:name]
+      end
+    end
+    result
+  end
+
+
+  def joes_large_dogs
+    result = []
+    @dogs.select do |dog_hash|
+      if dog_hash[:size] == :large && dog_hash[:owner][:name][:first] == 'Joe'
+        result << dog_hash[:name]
+      end
+    end
+    result
+  end
+
+  def sizes
+    size_arry = []
+    @dogs.each do |x|
+      size_arry << x[:size]
+    end
+    size_arry.uniq
+  end
+
+
+  def owners
+    owners_arry = []
+    @dogs.each do |x|
+      owners_arry << "#{x[:owner][:name][:first]} #{x[:owner][:name][:last]}"
+    end
+  owners_arry.uniq
+  end
+
+  def average_owners
+    result = []
+    @dogs.select do |dog_hash|
+      if dog_hash[:owner][:owner_quality] == AVERAGE
+        result << "#{dog_hash[:owner][:name][:first]} #{dog_hash[:owner][:name][:last]}"
+      end
+    end
+    result.uniq
+  end
+
+  def to_s
+    #just an array formed with new line breaks
+    #join method
+    value = @dogs.group_by do |dog|
+      dog[:owner]
+    end
+    puts value
+  end
 
 end
+
+
+
+
+
